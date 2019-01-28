bb47cb4f95c447e356c3ffed794dd1eb6ab377d1
Panos Mavrogiorgos <pmav99@gmail.com>
2019-01-28 12:46:52 +0200

    [files] Update the run scripts.

--- files/run_grass_gui.sh
+++
@@ -1,6 +0,0 @@
-#!/usr/bin/env bash
-#
-# Run the GRASS GUI, after activating the virtualenv
-
-source venv/bin/activate
-exec ./bin.x86_x64-pc-linux-gnu/grass77
--- files/run_gui.sh
+++ files/run_gui.sh
@@ -1,14 +1,10 @@
 #!/usr/bin/env bash
 #
-# Run the testsuite after activating the venv
-#
+# Run the GRASS GUI, after activating the virtualenv
 
 # the 'dirname readlink' line finds the directory where this script is located.
 # https://stackoverflow.com/questions/59895/getting-the-source-directory-of-a-bash-script-from-within
 GRASS_SRC="$(dirname $(readlink -f $0))"
-PATH="${PATH}:${GRASS_SRC}/bin.x86_64-pc-linux-gnu/"
 
 source "${GRASS_SRC}"/venv/bin/activate
-
-cd "${GRASS_SRC}"/testsuite/examples
-exec ./test_framework_GRASS_GIS_with_NC.sh test_framework_GRASS_GIS_with_NC.conf
+exec "${GRASS_SRC}"/bin.x86_64-pc-linux-gnu/grass77
