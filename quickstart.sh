1363888f9db86c5fcb7586ce6dc295da9379cfcc
Panos Mavrogiorgos <pmav99@gmail.com>
2019-01-28 16:11:44 +0200

    [quickstart] Use --force
    [skip ci]

--- quickstart.sh
+++ quickstart.sh
@@ -3,7 +3,7 @@
 # Quickstart!
 #
 
-ansible-galaxy install pmav99.compile_grass
+ansible-galaxy install --force pmav99.compile_grass
 wget https://git.io/fh8FY -O compile_grass.yml
 sed -i -e "s/builder/`whoami`/g" compile_grass.yml
 ansible-playbook compile_grass.yml --ask-become-pass
