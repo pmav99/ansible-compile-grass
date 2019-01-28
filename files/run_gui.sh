84e4c13cd5ab77596e72bb5da7ce05d250ab36a6
Panos Mavrogiorgos <pmav99@gmail.com>
2019-01-28 19:48:40 +0200

    [files] Pass arguments from run_gui.sh to the GRASS binary
    [skip ci]

--- files/run_gui.sh
+++ files/run_gui.sh
@@ -7,4 +7,4 @@
 GRASS_SRC="$(dirname $(readlink -f $0))"
 
 source "${GRASS_SRC}"/venv/bin/activate
-exec "${GRASS_SRC}"/bin.x86_64-pc-linux-gnu/grass77
+exec "${GRASS_SRC}"/bin.x86_64-pc-linux-gnu/grass77 "$@"
